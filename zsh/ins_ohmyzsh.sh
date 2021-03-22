1b06fbc4fde70973e9892789e33d6043fde5e471
charles-alex8888 <80571168+charles-alex8888@users.noreply.github.com>
2021-03-22 10:19:56 +0800

    Delete zsh directory

--- zsh/ins_ohmyzsh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-yum install zsh git -y  && \
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && \
-chsh -s /bin/zsh && sleep 2&& \
-sleep 3 && \
-sed -i "s/ZSH_THEME=.*/ZSH_THEME=\"agnoster\"/g" /root/.zshrc 
-
-
