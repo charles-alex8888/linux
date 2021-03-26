ffecc1df8097abdef75b21bd8fc54382fd2a1e93
charles-alex8888 <80571168+charles-alex8888@users.noreply.github.com>
2021-03-26 17:10:44 +0800

    Update iptables.sh

--- shell/iptables.sh
+++ shell/iptables.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
+iptables -P INPUT ACCEPT
+iptables -F
+  
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
 iptables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
 iptables -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
+
+service iptables save
