07822537d52cc0b3fd650f524c5fd14e6126c2dc
charles-alex8888 <80571168+charles-alex8888@users.noreply.github.com>
2021-05-04 13:45:40 +0800

    Update install_java.sh

--- install_app/install_java.sh
+++ install_app/install_java.sh
@@ -5,7 +5,7 @@ yum install -y java-1.8.0-openjdk-devel.x86_64
 URL=`ls -lrt /etc/alternatives/java | awk '{print $NF}'`
 JAVA_PATH=${URL%/jre*}
 ls -lrt /usr/bin/java 
-cat <<'EOF' > /etc/profile.d/jdk.sh
+cat <<EOF > /etc/profile.d/jdk.sh
 export JAVA_HOME=$JAVA_PATH
 export JRE_HOME=$JAVA_HOME/jre
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$JRE_HOME/lib:$CLASSPATH
